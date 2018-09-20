7b3eb31ba01e0f830c76f6e4ab29dfc267a81d30
Jasonnnngao <ziqing2@ualberta.ca>
2018-09-19 19:29:32 -0600

    lab2

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -2,9 +2,16 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
-public class Tweet {
+public abstract class Tweet {
     protected String message;
     protected Date date;
+    public Tweet(String message){
+        this.message=message;
+    }
+    public Tweet(String message,Date date){
+        this.message=message;
+        this.date= new Date();
+    }
 
     public void setMessage(String message) throws TooLongTweetException{
         if (message.length()>140) {
@@ -23,4 +30,6 @@ public class Tweet {
     public String getMessage() {
         return message;
     }
+    public abstract Boolean isImportant();
+
 }
